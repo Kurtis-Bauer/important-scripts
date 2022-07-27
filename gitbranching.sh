07a5230dc7da9120c05146d5b6337d99342c6647
Kurtis-Bauer <67176176+Kurtis-Bauer@users.noreply.github.com>
2022-07-26 20:26:34 -0400

    git branching commands

---
+++ gitbranching.sh
@@ -0,0 +1,8 @@
+# this section is for creating a new branch, and switching into it immediately
+git checkout -b branchname
+
+# this section is for checking what branches exit locally
+git branch
+
+# this is how you switch into a branch you are not in
+git checkout branchname
